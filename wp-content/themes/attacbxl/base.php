c93d3bcb4f2386c47fc6158e7cff0a7e706a8949
Boris Rorsvort <boris.rorsvort@gmail.com>
2015-01-31 17:21:29 +0100

    more styles and homepage design start

--- wp-content/themes/attacbxl/base.php
+++ wp-content/themes/attacbxl/base.php
@@ -11,23 +11,26 @@
   do_action('get_header');
   get_template_part('templates/header');
   ?>
-
-  <div class="container" role="document">
-    <div class="row">
-      <?php if (roots_display_sidebar()) : ?>
-        <main class="col s8" role="main">
-          <?php include roots_template_path(); ?>
-        </main>
-        <aside class="sidebar col s4" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside>
-      <?php else: ?>
-        <main class="col s12" role="main">
-          <?php include roots_template_path(); ?>
-        </main>
-      <?php endif; ?>
+  <?php if (is_front_page()): ?>
+    <?php include roots_template_path(); ?>
+  <?php else: ?>
+    <div class="container" role="document">
+      <div class="row">
+        <?php if (roots_display_sidebar()) : ?>
+          <main class="col s8" role="main">
+            <?php include roots_template_path(); ?>
+          </main>
+          <aside class="sidebar col s4" role="complementary">
+            <?php include roots_sidebar_path(); ?>
+          </aside>
+        <?php else: ?>
+          <main class="col s12" role="main">
+            <?php include roots_template_path(); ?>
+          </main>
+        <?php endif; ?>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 
 
   <?php get_template_part('templates/footer'); ?>
