786cfe9ef8d67e230005a9bebfbc99bb40a75a9f
Boris Rorsvort <boris.rorsvort@gmail.com>
2015-01-31 17:24:44 +0100

    new files

---
+++ wp-content/themes/attacbxl/front-page.php
@@ -0,0 +1,13 @@
+<div class="masthead" style='background-image: url(<?php echo home_url(); ?>/assets/img/homebg.jpg);'>
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <img src="<?php echo home_url(); ?>/assets/img/logo.png" alt="Attac Bruxelles 2: un autre monde est possible"/>
+        <h1>Un autre monde est possible</h1>
+        <h2 class="text-muted"><small>Réapproprions-nous ensemble l’avenir de notre société</small></h2>
+      </div>
+    </div>
+  </div>
+  <div class="masthead__mask" style='background-image: url(<?php echo home_url(); ?>/assets/img/homebg-mask.png);'>
+  </div>
+</div>
