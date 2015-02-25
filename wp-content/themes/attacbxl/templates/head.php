8c8b2ba348bee42c122577d2d000767668980150
Boris Rorsvort <boris.rorsvort@gmail.com>
2015-02-25 15:00:54 +0100

    adds facebook event importer

--- wp-content/themes/attacbxl/templates/head.php
+++ wp-content/themes/attacbxl/templates/head.php
@@ -8,4 +8,22 @@
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 
   <?php wp_head(); ?>
+
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '810881548998318',
+        xfbml      : true,
+        version    : 'v2.2'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  </script>
 </head>
