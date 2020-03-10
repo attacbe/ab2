69e31dd9fe1621f7c518409ef61b0b2d1b56db47
Boris Rorsvort <boris@rorsvort.com>
2020-03-10 21:53:59 +0100

    Add fallback for upcoming events

--- templates/components/upcoming-events.php
+++ templates/components/upcoming-events.php
@@ -4,20 +4,31 @@
   $all_events = tribe_get_events(array(
     // 'tribe_events_cat' => 'conferences',
     'order' => 'ASC',
-    'eventDisplay'=>'list',
-    'posts_per_page'=>-3
+    'eventDisplay' => 'list',
+    'posts_per_page' => -3
   ));
 
-  foreach($all_events as $post) {
+  foreach ($all_events as $post) {
     setup_postdata($post);
   ?>
-  <div class='media'>
-    <div class="media-body">
-      <?php echo the_event_start_date(); ?>
-      <a href="<?php the_permalink(); ?>" class='media-heading'>
-        <h5><?php the_title(); ?></h5>
-      </a>
+    <div class='media'>
+      <div class="media-body">
+        <?php echo the_event_start_date(); ?>
+        <a href="<?php the_permalink(); ?>" class='media-heading'>
+          <h5><?php the_title(); ?></h5>
+        </a>
+      </div>
     </div>
-  </div>
-  <?php } wp_reset_query(); ?>
+  <?php }
+
+  if (empty($all_events)) {
+  ?>
+    <div class='media'>
+      <div class="media-body">
+        Il n’y a pas d’évènements à venir
+      </div>
+    </div>
+  <?php
+  }
+  wp_reset_query(); ?>
 </div>
